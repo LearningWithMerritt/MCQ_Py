7db9d7b7cb116779db8b8f7fdc12887f7675b072
Evanx702 <evanmerrittscience@gmail.com>
2025-01-20 20:18:55 -0600

    update

--- app-cli/quiz/mcq.py
+++ app-cli/quiz/mcq.py
@@ -15,7 +15,6 @@ class MCQ():
         self.response:str = None
 
         self.randomize()
-        self.choice_map = self.map_choices()
 
     def __eq__(self, other):
         if isinstance(other, MCQ):
@@ -26,69 +25,67 @@ class MCQ():
         self.choices.append(self.answer)
         self.choices.shuffle()
 
-    def map_choices(self)-> dict[str:str]:
-        
-        choice_map: dict[str:str] = {}
-        
-        for i, e in enumerate(self.choices):
-            choice_map[chr(i+65)] = e
-
-        return choice_map
-    
     def check_answer(self,response) -> bool:
 
-        if(self.choice_map[response] == self.answer):
-            print("CORRECT!")
+        if(self.answer == response):
+            print("[CORRECT!]")
             return True
         else:
-            print("INCORRECT.")
+            print("[INCORRECT.]")
             return False
          
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    def make_prompt(self,header,number,footer) -> None:
-        self.prompt = f"{header}{number}.) {self.question}\n\n"
+    # def map_choices(self)-> dict[str:str]:
         
-        separator = "-" * int(len(header)/2)
+    #     choice_map: dict[str:str] = {}
+        
+    #     for i, e in enumerate(self.choices):
+    #         choice_map[chr(i+65)] = e
 
-        self.prompt += separator + "\n"
+    #     return choice_map
 
-        for key, value in self.choice_map.items():
-            self.prompt+= (f"{key}: {value}\n")
 
-        self.prompt += separator + "\n"
 
-        self.prompt += footer
+
+
+
+
+
+
+
+
+
+    # def make_prompt(self,header,number,footer) -> None:
+    #     self.prompt = f"{header}{number}.) {self.question}\n\n"
+        
+    #     separator = "-" * int(len(header)/2)
+
+    #     self.prompt += separator + "\n"
+
+    #     for key, value in self.choice_map.items():
+    #         self.prompt+= (f"{key}: {value}\n")
+
+    #     self.prompt += separator + "\n"
+
+    #     self.prompt += footer
 
 
          
-    def get_choice(self) -> str:
-        length = len(self.choices)
-        pattern = f"^[a-{chr(length+97)}qrA-{chr(length+65)}QR]" + r"{1}$"
+    # def get_choice(self) -> str:
+    #     length = len(self.choices)
+    #     pattern = f"^[a-{chr(length+97)}qrA-{chr(length+65)}QR]" + r"{1}$"
 
-        user_in = check_input(
-            pattern,
-            self.prompt,
-            f"Please type only single character from A-{chr(length+65)}",
-        )
+    #     user_in = check_input(
+    #         pattern,
+    #         self.prompt,
+    #         f"Please type only single character from A-{chr(length+65)}",
+    #     )
 
-        return user_in
+    #     return user_in
     
-    def set_number(self,n)-> None:
-        self.num_str = str(n) +".) "
+    # def set_number(self,n)-> None:
+    #     self.num_str = str(n) +".) "
 
 
 
--- app-cli/utils/file_hander.py
+++ app-cli/utils/file_hander.py
@@ -3,17 +3,100 @@ import json
 from pathlib import Path
 from datetime import datetime
 
-def write_json(path, data):
-    with open(path, "w") as file:
-        json.dump(data,file,indent=4)
 
-def read_json(path):
-    with open(path, "r") as file:
-        data = json.load(file)
+class JSON_Handler():
+    
+    def __init__(self, path):
+        self.path = path
+        self.parent = path.parent
 
-    return data
+    def read(self):
+        with open(self.path, "r") as file:
+            data = json.load(file)
 
-def delete_fromdir(path,extension=None):
+        return data
+
+    def write(self, data, indent=4):
+        with open(self.path, "w") as file:
+            json.dump(data, file, indent=indent)
+
+
+    def create(self):
+        if not os.path.exists(self.path):
+            self.write({})
+
+    def delete(self):
+        if os.path.exists(self.path):
+            self.path.unlink()
+
+class Save(JSON_Handler):
+    def __init__(self, path):
+        super().__init__(path)
+
+
+    def read(self):
+        '''TODO:
+            decrypt file
+            read data
+            delete plain file
+            return data
+        '''
+        pass
+
+    def write(self):
+        '''TODO:
+            decrypt file
+            read data
+            write data
+            write file
+            encrypt file
+            delete plain
+        '''
+        pass
+
+class Xor():
+    def __init__(self, path):
+        self.path = path
+        self.parent = path.parent
+        
+    def gen_key(self, length):
+        return os.urandom(length)
+
+    def xor(self, data, key):
+        return bytes(a ^ b for a,b in zip(data,key))
+
+    def enxor(self, inpath, outpath, keypath):
+        with open(inpath,"rb") as file:
+            data = file.read()
+
+        key = self.gen_key(len(data))
+        with open(keypath, "wb") as file:
+            file.write(key)
+
+        cipher_data = self.xor(data,key)
+        with open(outpath, "wb") as file:
+            file.write(cipher_data)
+
+    def dexor(self, inpath, outpath, keypath):
+        with open(inpath,"rb") as file:
+            data = file.read()
+
+        with open(keypath, "rb") as file:
+            key = file.read()
+
+        plain = self.xor(data,key)
+        with open(outpath, "wb") as file:
+            file.write(plain)
+
+
+
+
+
+
+
+
+
+def delete_files(path,extension=None):
     for file in path.iterdir():
         if file.is_file():
             if extension is None:
@@ -31,5 +114,3 @@ def log(info):
     with open(path / ".log", "ab") as file:
         file.write(bytes(f"{time} : {info}\n".encode("utf-8")))
 
-if __name__ == "__main__":
-    log("testing log")
